afc3af7efb7c583131238fbb6ef931d36bd630b0
John Larson <john.robert.larson@gmail.com>
2018-02-07 18:09:15 -0700

    initial update

--- app.js
+++ app.js
@@ -22,11 +22,21 @@ function getViolations() {
 }
 
 function updateCurrentTemp(temp) {
-
+	$('#current-temp').text(`${temp} °F`);
 }
 
 function updateTempsList(temps, violations) {
-
+	const violationTimes = new Set();
+	for(let violation of violations) {
+		violationTimes.add(violation.timestamp);
+	}
+	$('#log-list').empty();
+	for(let temp of temps) {
+		const tooHot = violationTimes.has(temp.timestamp);
+		const cls = tooHot ? 'violation' : '';
+		const item = $(`<li class="${cls}">${temp.timestamp} -- ${temp.temperature} °F</li>`)
+		$('#log-list').append(item);
+	}
 }
 
 function query(ruleset, name, args = {}) {
