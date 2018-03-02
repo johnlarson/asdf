a9e0d49cc0820b51398c6e71f4959ffff04ab3b8
John Larson <john.robert.larson@gmail.com>
2018-03-01 18:15:12 -0700

    test stubs

---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class Tests(unittest.TestCase):
+
+    def test_create_delete(self):
+        """Tests sensor create and delete functionality."""
+        ...
+
+    def test_sensor_new_temperature_event(self):
+        """
+        Sensors created should respond correctly to new temperature events.
+        """
+        ...
+
+    def test_sensor_profile(self):
+        """Sensor profile should get set reliably."""
+        ...
