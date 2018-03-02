bdafa42f04754830009ff81db121247ce08110f2
John Larson <john.robert.larson@gmail.com>
2018-03-01 19:30:05 -0700

    changes

--- test.py
+++ test.py
@@ -11,12 +11,14 @@ class Pico:
     def __init__(self, id_):
         self.id = id_
 
-    def get(self, ruleset, func, **attrs):
+    def get(self, func, **attrs):
+        ruleset, func = func.split(':')
         url = 'http://localhost:8080/sky/cloud/{}/{}/{}/'
         url = url.format(self.id, ruleset, func)
         return requests.get(url, data=attrs).json()
 
-    def fire(self, domain, event, **attrs):
+    def fire(self, event, **attrs):
+        domain, event = event.split(':')
         url = 'http://localhost:8080/sky/event/{}/test/{}/{}/'
         url = url.format(self.id, domain, event)
         return requests.get(url, data=attrs).json()
@@ -29,7 +31,7 @@ class Tests(unittest.TestCase):
 
     def test_create_delete(self):
         """Tests sensor create and delete functionality."""
-        print(self.manager.get('manage_sensors', 'sensors'))
+        print(self.manager.get('manage_sensors:sensors'))
 
     def test_sensor_new_temperature_event(self):
         """
