37692517934e2831ee6fa67b25d9c7591fab02ad
John Larson <john.robert.larson@gmail.com>
2018-03-01 19:26:13 -0700

    Pico for tests

--- test.py
+++ test.py
@@ -1,11 +1,35 @@
 import unittest
 
+import requests
+
+
+MANAGER_CHANNEL = '7EtK8TRBRYVjYg2BEn3Yeb'
+
+
+class Pico:
+
+    def __init__(self, id_):
+        self.id = id_
+
+    def get(self, ruleset, func, **attrs):
+        url = 'http://localhost:8080/sky/cloud/{}/{}/{}/'
+        url = url.format(self.id, ruleset, func)
+        return requests.get(url, data=attrs).json()
+
+    def fire(self, domain, event, **attrs):
+        url = 'http://localhost:8080/sky/event/{}/test/{}/{}/'
+        url = url.format(self.id, domain, event)
+        return requests.get(url, data=attrs).json()
+
 
 class Tests(unittest.TestCase):
 
+    def setUp(self):
+        self.manager = Pico(MANAGER_CHANNEL)
+
     def test_create_delete(self):
         """Tests sensor create and delete functionality."""
-        ...
+        print(self.manager.get('manage_sensors', 'sensors'))
 
     def test_sensor_new_temperature_event(self):
         """
